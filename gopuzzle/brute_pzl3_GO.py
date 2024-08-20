05a11f56c0cdae45f4bb935e29e54fce02dfebaf
Rivaldiananto <rivaldiananto@gmail.com>
2024-08-20 21:07:11 +0700

    Update brute_pzl3_GO.py

--- gopuzzle/brute_pzl3_GO.py
+++ gopuzzle/brute_pzl3_GO.py
@@ -5,6 +5,7 @@ from threading import Event
 
 import hashlib
 
+import tempfile
 import subprocess
 
 from datetime import datetime
@@ -13,15 +14,6 @@ def log_str(logfile_name, string_to_log):
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         f.write(dt_string+': '+string_to_log+'\n')
-
-def sha512_11512(key):
-    n = 1 + 11512
-    key_i = key.encode('utf-8')
-    for i in range(0, n-1):
-        key_i = hashlib.sha512(key_i).digest()
-    key_i = hashlib.sha512(key_i).hexdigest()
-    
-    return key_i
 
 
 def t_job(t_id, T_FOUND_ev, left, right, answers_list, B_A_ev):
@@ -34,12 +26,13 @@ def t_job(t_id, T_FOUND_ev, left, right, answers_list, B_A_ev):
     
     prepared_answer = []
     for Ib in range(0, (right - left) // BATCH_SIZE + 1):
-        solution = -1        
+        solution = -1
+        
         
         prepared_answer = answers_list[(left + Ib*BATCH_SIZE) : min(left + (Ib+1)*BATCH_SIZE, right+1)]
 
             # Create a subprocess and set up pipes for input and output
-        process = subprocess.Popen(["./pzl3_GOscript"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        process = subprocess.Popen(["./pzl5_GOscript"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
 
             # Prepare the data to be sent to the subprocess
         data = "\n".join(prepared_answer)  # Join your prepared answers with newlines
@@ -75,14 +68,14 @@ def t_job(t_id, T_FOUND_ev, left, right, answers_list, B_A_ev):
         B_A_ev.wait()
         log_str('./LOG/main.log', f'[{t_id}] Hope THE B.A. to use')
         B_A_ev.clear()
-        with open('pzl3_bruttedAnswers.txt', 'a') as f:
+        with open('pzl5_bruttedAnswers.txt', 'a') as f:
             f.write('\n'.join(answers_list[(left + Ib*BATCH_SIZE):(min(left + (Ib+1)*BATCH_SIZE, right+1))])+'\n')
         log_str('./LOG/main.log', f'[{t_id}] ....THE B.A. released to be used')
         B_A_ev.set()
         
         prepared_answer.clear()
             
-        with open('failsafe_AR_3.txt', 'r') as f:
+        with open('failsafe_AR_5.txt', 'r') as f:
             failsafe_file_closing_flag = int(next(f))
                 
         if failsafe_file_closing_flag:
@@ -96,47 +89,32 @@ def t_job(t_id, T_FOUND_ev, left, right, answers_list, B_A_ev):
 
 
 
-
     # Create DIR for LOGs if not exist
 if not os.path.exists("./LOG/"):
    os.makedirs("./LOG/")
-   
     # Read the keys
 log_str(f'./LOG/main.log', 'Reading the list of keys.....')           
 keys_list = []
-with open('pzl3_KEYS.txt') as f:
-    keys_list = [ list(set(l.strip('\n').lower().split(','))) for l in f.readlines()]
+with open('pzl5_KEYS.txt') as f:
+    keys_list = [ list(set(l.strip('\n').split(','))) for l in f.readlines()]
 log_str(f'./LOG/main.log', '.........keys have been read.')
 
-def filter_answers(answers_list):
-    if os.path.exists('pzl3_bruttedAnswers.txt'):
-        brutted_answers = set()
-        with open('pzl3_bruttedAnswers.txt') as f:
-            for line in f:
-                brutted_answers.add(line.strip('\n'))
-                if len(brutted_answers) == 10_000_000:
-                        # Process the lines in this chunk
-                    filtered_answers = [ans for ans in answers_list if ans not in brutted_answers]
-                    answers_list.clear()
-                    answers_list.extend(filtered_answers)
-                    
-                        # Clear the lines list to start a new chunk
-                    brutted_answers.clear()
+    # Read the brutted answers
+log_str(f'./LOG/main.log', 'Reading the brutted answers.....')           
+brutted_answers = set()
+if os.path.exists('pzl5_bruttedAnswers.txt'):
+    with open('pzl5_bruttedAnswers.txt') as f:
+        brutted_answers = {l.strip('\n') for l in f.readlines()}
+log_str(f'./LOG/main.log', '.........brutted answers have been read.')
 
-                # Process remaining brutted answers
-            if brutted_answers:
-                filtered_answers = [ans for ans in answers_list if ans not in brutted_answers]
-                answers_list.clear()
-                answers_list.extend(filtered_answers)
-                
-            brutted_answers.clear()
-
+    # Preapare the semaphore for BRUTTED ANSWERS database (file)
+B_A_ev = Event()
+B_A_ev.set()
 
     # Prepare the list of answers to be brutted
 skipped_counter = 0
 log_str(f'./LOG/main.log', 'Preparing the list of answers....')  
 answers_list = []
-temp_answers = []
 for k1 in keys_list[0]:
     for k2 in keys_list[1]:
         for k3 in keys_list[2]:
@@ -147,23 +125,12 @@ for k1 in keys_list[0]:
                             for k8 in keys_list[7]:
                                 for k9 in keys_list[8]:
                                     for k10 in keys_list[9]:
-                                        temp_answers.append(''+k1+k2+k3+k4+k5+k6+k7+k8+k9+k10)
-                                        if len(temp_answers) >= 5_000_000:
-                                            before_len = len(temp_answers)
-                                            filter_answers(temp_answers)
-                                            answers_list.extend(temp_answers)
-                                            after_len = len(temp_answers)
-                                            temp_answers.clear()
-                                            skipped_counter += (before_len - after_len)
-                                            print(f"b:{before_len} a:{after_len}")
-if(temp_answers):
-    before_len = len(temp_answers)
-    filter_answers(temp_answers)
-    answers_list.extend(temp_answers)
-    after_len = len(temp_answers)
-    skipped_counter += (before_len - after_len)
-    print(f"b:{before_len} a:{after_len}")
-    
+                                        answer = ''+k1+k2+k3+k4+k5+k6+k7+k8+k9+k10
+                                        if answer in brutted_answers:
+                                            skipped_counter += 1
+                                        else:
+                                            answers_list.append(answer)
+                                            
 log_str(f'./LOG/main.log', '............ answers list prepared')                             
 
 log_str(f'./LOG/main.log', f'{len(answers_list)} answers to be brutted, {skipped_counter} skipped (already been brutted)')                             
@@ -171,16 +138,11 @@ log_str(f'./LOG/main.log', f'{len(answers_list)} answers to be brutted, {skipped
 
 log_str(f'./LOG/main.log', f"Total size of the answers list is: {len(answers_list)}") 
 
-
-    # Preapare the semaphore for BRUTTED ANSWERS database (file)
-B_A_ev = Event()
-B_A_ev.set()
-
 t_num = 8
 t = [0] * t_num
 T_FOUND_ev = Event()
 T_FOUND_ev.clear()
-with open('failsafe_AR_3.txt', 'w') as f:
+with open('failsafe_AR_5.txt', 'w') as f:
     f.write('%d' % 0)
     
 solution_space_size = len(answers_list)
@@ -195,11 +157,3 @@ for i in range(t_num):
     t[i].join()
        
 print('FINISH')
-
-
-
-
-
-
-
-
